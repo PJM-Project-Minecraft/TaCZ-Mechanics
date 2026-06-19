8fe285b0036b30de3cefdabf50a4aae3df175164
Liko <nikitasamp030409@gmail.com>
2026-06-19 22:47:05 +0300

    test: add JUnit 5 test infrastructure

---
+++ src/test/java/ru/liko/tacz_mechanics/SmokeTest.java
@@ -0,0 +1,11 @@
+package ru.liko.tacz_mechanics;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+    @Test
+    void junitWorks() {
+        assertEquals(4, 2 + 2);
+    }
+}
